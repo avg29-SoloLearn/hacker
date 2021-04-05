ae0a821ca8cbec09b89ba624273322a67f5e93bc
avg29-SoloLearn <81770964+avg29-SoloLearn@users.noreply.github.com>
2021-04-05 11:59:40 +0300

    Update and rename README.md to hacker.py

---
+++ hacker.py
@@ -0,0 +1,4 @@
+Print("как написать файл с кодом в termux(Android) или cmd(windows/mac)?");
+Print("1. Зайдите в termux/cmd");
+Print("2. Если у вас android напишите команду $termux-setup-storage");
+Print("3. Напишите команду $Echo [код] >> [имя файла]");
